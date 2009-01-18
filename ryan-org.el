1a50546d722cccb42819c8cad5f47692321e1de6
Ryan McGuire <ryan@enigmacurry.com>
2009-01-18 16:01:27 -0500

    Org mode should be automatically enabled for .org.txt files

--- ryan-org.el
+++ ryan-org.el
@@ -1,4 +1,5 @@
 ;; Org-mode settings
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(add-to-list 'auto-mode-alist '("\\.org.txt$" . org-mode))
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
