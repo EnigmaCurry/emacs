823d2a73d4c13801985f34ac523026306992c7d4
Ryan McGuire <ryan@enigmacurry.com>
2009-01-23 18:56:36 -0500

    Removed non-functioning flyspell line from ryan-vc

--- ryan-vc.el
+++ ryan-vc.el
@@ -1,5 +1,3 @@
-(add-hook 'vc-logentry-check-hook 'flyspell-mode)
-
 (defadvice log-edit-done (around enforce-non-blank-log-message activate)
   "Enforce that commit logs are not blank"
   (if (not (string-match "\\w" (buffer-string)))
