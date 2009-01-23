ce79713e607d114a35b55acbc2192e9db8d6925b
Ryan McGuire <ryan@enigmacurry.com>
2009-01-23 16:21:13 -0500

    Advice to enforce non-blank commit messages.
    This commit message was the final test ;)

--- ryan-vc.el
+++ ryan-vc.el
@@ -2,6 +2,6 @@
 
 (defadvice log-edit-done (around enforce-non-blank-log-message activate)
   "Enforce that commit logs are not blank"
-  (if (string-match "\\w" (buffer-string))
+  (if (not (string-match "\\w" (buffer-string)))
       (message "Commit message must not be blank.")
     ad-do-it))
