239e637c685f48c04c51817b2ad60f5f7dd71058
Ryan McGuire <ryan@enigmacurry.com>
2009-05-21 11:24:03 -0400

    introduced SlickCopy functionality from: http://www.emacswiki.org/emacs/SlickCopy

--- ryan-text-mode.el
+++ ryan-text-mode.el
@@ -1,3 +1,18 @@
 ;This is for general text mode behaviour
 
 (setq fill-column 79)
+
+;Slick Copy is cool: http://www.emacswiki.org/emacs/SlickCopy
+(defadvice kill-ring-save (before slick-copy activate compile)
+  "When called interactively with no active region, copy a single line instead."
+  (interactive
+   (if mark-active (list (region-beginning) (region-end))
+     (message "Copied line")
+     (list (line-beginning-position)
+           (line-beginning-position 2)))))
+(defadvice kill-region (before slick-cut activate compile)
+  "When called interactively with no active region, kill a single line instead."
+  (interactive
+   (if mark-active (list (region-beginning) (region-end))
+     (list (line-beginning-position)
+           (line-beginning-position 2)))))
