cd60a50b319841ce60fcce0948c77d15ebd8b592
Ryan McGuire <ryan@enigmacurry.com>
2017-09-04 14:37:47 -0400

    literate emacs config

---
+++ init.el
@@ -0,0 +1,39 @@
+;; Prevent garbage collection on startup
+(setq gc-cons-threshold 400000000)
+
+;;; Begin initialization
+;; Turn off mouse interface early in startup to avoid momentary display
+(when t ;window-system
+  (menu-bar-mode -1)
+  (tool-bar-mode -1)
+  (scroll-bar-mode -1)
+  (tooltip-mode -1))
+
+(setq inhibit-startup-message t)
+(setq initial-scratch-message "")
+
+;;; Set up package
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+(package-initialize)
+
+;;; Bootstrap use-package
+;; Install use-package if it's not already installed.
+;; use-package is used to configure the rest of the packages.
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; From use-package README
+(eval-when-compile
+  (require 'use-package))
+(require 'diminish)                ;; if you use :diminish
+(require 'bind-key)
+;; (setq use-package-verbose t)
+
+;;; Load the config
+(org-babel-load-file (concat user-emacs-directory "config.org"))
+
+;; Reset normalish garbag collection setting:
+(setq gc-cons-threshold 800000)
