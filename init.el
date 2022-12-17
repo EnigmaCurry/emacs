d8842f4ed3e119cb229a6759c15180b581517ec0
Ryan McGuire <ryan@enigmacurry.com>
2022-12-17 13:38:05 -0500

    typescript

--- init.el
+++ init.el
@@ -418,6 +418,26 @@
 
 (use-package dockerfile-mode)
 
+;; typescript
+(use-package tide
+  :init
+  (defun setup-tide-mode ()
+    (interactive)
+    (tide-setup)
+    (flycheck-mode +1)
+    (setq flycheck-check-syntax-automatically '(save mode-enabled))
+    (eldoc-mode +1)
+    (tide-hl-identifier-mode +1)
+    ;; company is an optional dependency. You have to
+    ;; install it separately via package-install
+    ;; `M-x package-install [ret] company`
+    (company-mode +1))
+  ;; aligns annotation to the right hand side
+  (setq company-tooltip-align-annotations t)
+  ;; formats the buffer before saving
+  (add-hook 'before-save-hook 'tide-format-before-save)
+  (add-hook 'typescript-mode-hook #'setup-tide-mode))
+
 ;; Start server
 (require 'server)
 (unless (server-running-p)
