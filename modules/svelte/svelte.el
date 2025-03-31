e5aa14c2154141dcbc6e6f6892477bce6a63678d
EnigmaCurry <ryan@enigmacurry.com>
2025-03-31 11:15:52 -0400

    svelte prettier with apheleia

--- modules/svelte/svelte.el
+++ modules/svelte/svelte.el
@@ -2,11 +2,18 @@
   :straight t
   :mode ("\\.svelte\\'" . svelte-mode)
   :init
-  ;; Ensure lsp-mode and prettier-js are installed
-  (use-package prettier-js
+  (use-package lsp-mode
     :straight t)
-  :hook ((svelte-mode . lsp)           ; Start LSP when opening a Svelte file
-         (svelte-mode . prettier-js-mode)) ; Enable Prettier formatting
+  (use-package apheleia
+    :straight t)
+  :hook ((svelte-mode . lsp))  ; Start LSP when opening a Svelte file
   :config
-  ;; Optional additional settings (e.g., enable snippets in LSP)
-  (setq lsp-enable-snippet t))
+  (setq lsp-enable-snippet t)
+  
+  ;; Configure Apheleia to format Svelte files using Prettier v3.
+  ;; This command passes the current file path and forces the "svelte" parser.
+  (setf (alist-get 'svelte-mode apheleia-formatters)
+        '("prettier" "--stdin-filepath" filepath "--parser" "svelte"))
+  (setf (alist-get 'svelte-mode apheleia-mode-alist) 'svelte-mode)
+  
+  (apheleia-global-mode +1))
