cb3c2e90101438a9bce923cb127a9dc4f952dfae
EnigmaCurry <ryan@enigmacurry.com>
2026-05-28 09:09:22 -0600

    clojure

--- modules/clojure/clojure.el
+++ modules/clojure/clojure.el
@@ -1,1 +1,12 @@
-(use-package clojure-mode)
+(use-package clojure-mode
+  :hook
+  ((clojure-mode . subword-mode))
+  :init
+  (use-package cider)
+  (defun my/cljfmt-buffer ()
+    (interactive)
+    (when buffer-file-name
+      (shell-command (format "cljfmt fix %s"
+                             (shell-quote-argument buffer-file-name)))
+      (revert-buffer :ignore-auto :noconfirm)))
+  (use-package parinfer-rust-mode))
